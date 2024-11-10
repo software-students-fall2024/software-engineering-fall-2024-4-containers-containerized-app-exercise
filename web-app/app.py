6a0ae0da2ea125b7f13496e38636e34a58d7c9a4
ianpompliano <inp9194@nyu.edu>
2024-11-10 15:53:03 -0500

    added statistics page

--- web-app/app.py
+++ web-app/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, jsonify, request
+from save_data import save_to_mongo
+from get_statistics import get_statistics
 import logging
 import requests
-
-from save_data import save_to_mongo
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -14,13 +14,9 @@ def main_page():
    return render_template('main.html')
 
 
-@app.route('/save_results', methods=['POST'])
-def save_results_route():
-    
-    data = request.json
-    save_to_mongo(data)
-    
-    return '', 204
+@app.route('/statistics')
+def statistics_page():
+    return render_template('statistics.html')
 
 
 @app.route('/classify', methods=['POST'])
@@ -37,6 +33,21 @@ def classify():
     else:
         return {"error": "Classification failed"}, 500
     
+    
+@app.route('/save-results', methods=['POST'])
+def save_results_route():
+    
+    data = request.json
+    save_to_mongo(data)
+    
+    return '', 204
+    
+
+@app.route('/get-stats', methods=['GET'])
+def get_stats():
+    
+    return get_statistics()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ web-app/get_statistics.py
@@ -0,0 +1,73 @@
+import pymongo
+import os
+import certifi 
+from dotenv import load_dotenv
+import logging
+from flask import request
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+
+mongo_cxn = os.getenv('MONGO_CXN_STRING')
+logger.info(mongo_cxn)
+
+client = pymongo.MongoClient(mongo_cxn, tlsCAFile=certifi.where())
+
+db = client['project4']
+collection = db['num_classifications']
+
+def get_statistics():
+    try:
+        # get total count of documents
+        total_count = collection.count_documents({})
+
+        # get total number of correct predictions
+        correct_predictions = collection.count_documents(
+            {"$expr": {"$eq": ["$intended_num", "$classified_num"]}}
+        )
+        
+        # calculate total accuracy
+        overall_accuracy = (correct_predictions / total_count) * 100
+
+        # calculate accuracy for each digit
+        individual_digit_stats = {}
+        for digit in range(10): 
+            
+            # count total instances of this digit being drawn
+            total_digit = collection.count_documents({"intended_num": digit})
+            if total_digit > 0:
+                
+                # count correct classifications for this digit
+                correct_digit = collection.count_documents({
+                    "intended_num": digit,
+                    "classified_num": digit
+                })
+                
+                # calculate accuracy for this digit
+                digit_accuracy = (correct_digit / total_digit) * 100
+                
+                # save to output dict
+                individual_digit_stats[digit] = {
+                    "total_attempts": total_digit,
+                    "correct_classifications": correct_digit,
+                    "accuracy": round(digit_accuracy, 2)
+                }
+            else:
+                individual_digit_stats[digit] = {
+                    "total_attempts": 0,
+                    "correct_classifications": 0,
+                    "accuracy": 0
+                }
+
+        return {
+            "total_samples": total_count,
+            "correct_predictions": correct_predictions,
+            "overall_accuracy": round(overall_accuracy, 2),
+            "individual_digits": individual_digit_stats
+        }
+        
+    except Exception as e:
+        logger.error(f"Error calculating statistics: {str(e)}")
+        return {"error": f"Failed to calculate statistics: {str(e)}"}
