053f7b43e2c197fa72a14ac563e69ab120e38e62
BlackCloud-k <niyujian2004@126.com>
2024-11-10 23:35:42 -0500

    edit the speech to text function return

--- speech_to_text.py
+++ speech_to_text.py
@@ -20,7 +20,7 @@ def get_google_cloud_credentials():
 def transcribe_file(audio_file: str, credentials) -> speech.RecognizeResponse:
     try:
         client = speech.SpeechClient(credentials=credentials)
-        print(f"Reading audio file: {audio_file}")
+        # print(f"Reading audio file: {audio_file}")
         with open(audio_file, "rb") as f:
             audio_content = f.read()
         
@@ -32,17 +32,13 @@ def transcribe_file(audio_file: str, credentials) -> speech.RecognizeResponse:
             language_code="en-US",
         )
 
-        print("Sending recognition request...")
+        # print("Sending recognition request...")
         response = client.recognize(config=config, audio=audio)
 
-        # print the result
-        if response.results:
-            for result in response.results:
-                print(f"Transcript: {result.alternatives[0].transcript}")
-        else:
+        if not response.results:
             print("No transcription results found.")
 
-        return response
+        return response.results[0].alternatives[0]
 
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -50,4 +46,25 @@ def transcribe_file(audio_file: str, credentials) -> speech.RecognizeResponse:
 
 if __name__ == '__main__':
     credentials = get_google_cloud_credentials()
-    transcribe_file("recording.wav", credentials)
+    res = transcribe_file("recording.wav", credentials)
+    print(res)
+
+
+'''
+Sample result:
+results {
+  alternatives {
+    transcript: "hello may I ask what\'s your name"
+    confidence: 0.913279235
+  }
+  result_end_time {
+    seconds: 3
+    nanos: 640000000
+  }
+  language_code: "en-us"
+}
+total_billed_time {
+  seconds: 4
+}
+request_id: 4550153494959018094
+'''
