65b5bb47867b5ec3d5b54489e69922cdb51650e8
reyhanquayum <reyhan.quayum@gmail.com>
2024-11-11 21:18:27 -0500

    docker + mongo setup

---
+++ web-app/app.py
@@ -0,0 +1,7 @@
+from flask import Flask, jsonify
+from pymongo import MongoClient
+app = Flask(__name__)
+client = MongoClient("mongodb://mongodb:27017/")
+db = client["financiersDB"]
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
