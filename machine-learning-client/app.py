5121abb23ee2d436ed39776d017b34486f33c598
imyhalex <ys4689@nyu.edu>
2024-11-12 02:30:55 -0500

    ML client

---
+++ machine-learning-client/app.py
@@ -0,0 +1,69 @@
+import os
+import uuid
+import nltk
+from nltk.tokenize import sent_tokenize
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from datetime import datetime
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+# step 1: retrive the data in this structure
+# {
+#     "_id": ObjectId("..."),
+#     "request_id": "unique_request_id",
+#     "sentences": [
+#         {
+#             "sentence": "Sentence 1.",
+#             "status": "pending",
+#             "analysis": null
+#         },
+#         {
+#             "sentence": "Sentence 2.",
+#             "status": "pending",
+#             "analysis": null
+#         },
+#         // ... more sentences
+#     ],
+#     "overall_status": "pending",   // Indicates the status of the entire submission
+#     "timestamp": ISODate("...")    // Timestamp of the submission
+# }
+
+# step 2: do analysis and store Compond, Neutural, Postive, and Negative metircs into analysis field
+# step 3: do Topic Modeling, Emotion Detection, Text Summarization, Sentiment Trend Analysis
+
+load_dotenv()
+
+"""This part is for seeding data"""
+# nltk.download("punkt")
+# nltk.download("punkt_tab")
+
+# with open("speech.txt", "r", encoding="utf-8") as f:
+#     speech_text = f.read()
+
+# sentences = sent_tokenize(speech_text)
+# request_id = str(uuid.uuid4())
+# document = { 
+#     "request_id": request_id,
+#     "sentences": [],
+#     "overall_status": "pending",
+#     "timestamp": datetime.now()
+# }
+
+# for sentence in sentences:
+#     sentence_entry = { 
+#         "sentence": sentence.strip(),
+#         "status": "pending",
+#         "analysis": None
+#     }
+#     document["sentences"].append(sentence_entry)
+
+mongo_uri = os.getenv("MONGO_URI")
+client = MongoClient(mongo_uri)
+db = client["sentiment"]
+texts_collection = db["texts"]
+
+"""Seed data"""
+# result = texts_collection.insert_one(document)
+# print(f"Document inserted with _id: {result.inserted_id} and request_id: {request_id}")
+
+
