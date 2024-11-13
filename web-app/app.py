411184b7c15bf44763d83a5b72c15f5255a23f5e
imyhalex <ys4689@nyu.edu>
2024-11-12 21:56:05 -0500

    udpate

--- web-app/app.py
+++ web-app/app.py
@@ -18,12 +18,6 @@ db = client["sentiment"]  # Database name
 collection = db["texts"]  # Collection name
 
 
-mongo_uri = os.getenv("MONGO_URI")
-client = MongoClient(mongo_uri)  # Adjust the connection string if necessary
-db = client["sentiment"]  # Database name
-collection = db["texts"]  # Collection name
-
-
 @app.route("/")
 def index():
     return render_template("index.html")
