c40f4f5e6ba60759d851f239fb73b9f02fb366f5
JosephChege4 <chegejrmungai@gmail.com>
2024-11-13 15:15:26 -0500

    reset

---
+++ web-app/test_app.py
@@ -0,0 +1,29 @@
+"""
+Unit tests for app.py web app.
+"""
+
+import pytest
+from app import app
+
+
+@pytest.fixture
+def app_client():
+    """Create a test client for the Flask app."""
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_index_route(client):
+    """Test the index route."""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Tap to Listen" in response.data
+
+
+# If we use `/results`
+# def test_results_route(client):
+#     """Test the results route."""
+#     response = client.get("/results")
+#     assert response.status_code == 200
+#     assert b"Classification Result" in response.data
