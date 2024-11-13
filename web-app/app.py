974cbcf5f85b7957e77f6b5f16d4ae028f3a0357
JosephChege4 <chegejrmungai@gmail.com>
2024-11-13 17:30:25 -0500

    Override local file with remote version

--- web-app/app.py
+++ web-app/app.py
@@ -7,22 +7,18 @@ This Flask app provides a web interface for recording audio and displaying class
 from flask import Flask, render_template
 
 
-def launch_app():
-    """Get the app launched"""
-    app = Flask(__name__)  # pylint: disable=invalid-name
+app = Flask(__name__)  # pylint: disable=invalid-name
 
-    @app.route("/")
-    def index():
-        """Render the index page."""
-        return render_template("index.html")
 
-    # @app.route('/results')
-    # def results():
-    #     return render_template('results.html')
+@app.route("/")
+def index():
+    """Render the index page."""
+    return render_template("index.html")
 
-    return app
 
+# @app.route('/results')
+# def results():
+#     return render_template('results.html')
 
 if __name__ == "__main__":
-    client_app = launch_app()
-    client_app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
--- web-app/test_app.py
+++ web-app/test_app.py
@@ -3,20 +3,19 @@ Unit tests for app.py web app.
 """
 
 import pytest
-from app import launch_app
+from app import app
 
 
 @pytest.fixture
 def app_client():
     """Create a test client for the Flask app."""
-    app = launch_app()
     with app.test_client() as client:
         yield client
 
 
-def test_index_route(app_client):
+def test_index_route(client):
     """Test the index route."""
-    response = app_client.get("/")
+    response = client.get("/")
     assert response.status_code == 200
     assert b"Tap to Listen" in response.data
 
