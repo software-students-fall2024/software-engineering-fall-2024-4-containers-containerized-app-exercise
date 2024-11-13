093da9f434ad6f5337d093c7d35c692a120bfb51
dangelo729 <peterdangelo729@gmail.com>
2024-11-12 19:57:23 -0500

    created webapp, added docker-compose.yml, adjsuted ml to work with webapp

---
+++ web-app/app.py
@@ -0,0 +1,28 @@
+
+"""
+Web application for sound classification.
+
+This Flask app provides a web interface for recording audio and displaying classification results.
+"""
+
+from flask import Flask, render_template 
+
+
+app = Flask(__name__)  # pylint: disable=invalid-name
+
+
+
+@app.route("/")
+def index():
+    """Render the index page."""
+    return render_template("index.html")
+
+
+# Route to handle results display if needed
+# @app.route('/results')
+# def results():
+#     # Logic to display results
+#     return render_template('results.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
