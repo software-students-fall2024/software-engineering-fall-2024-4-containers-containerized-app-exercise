608040c3a8936cabd3087c93113bd535bf09250d
JosephChege4 <chegejrmungai@gmail.com>
2024-11-13 15:55:13 -0500

    pytest-flask fixes

--- web-app/test_app.py
+++ web-app/test_app.py
@@ -3,20 +3,25 @@ Unit tests for app.py web app.
 """
 
 import pytest
-from app import app
+from app import app as flask_app
+
+
+def app():
+    """Pytest-flask handling."""
+    flask_app.config["TESTING"] = True
+    return flask_app
 
 
 @pytest.fixture
 def app_client():
     """Create a test client for the Flask app."""
-    app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
 
 
 def test_index_route(client):
     """Test the index route."""
-    response = client.get("/")
+    response = app_client.get("/")
     assert response.status_code == 200
     assert b"Tap to Listen" in response.data
 
