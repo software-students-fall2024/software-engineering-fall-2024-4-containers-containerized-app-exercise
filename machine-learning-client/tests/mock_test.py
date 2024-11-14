8e72c06feaa98c37611f47ea4250e37f96d30577
LucaIgnatescu <lucaignatescu003@gmail.com>
2024-11-14 10:23:29 -0500

    added env test

--- machine-learning-client/tests/mock_test.py
+++ machine-learning-client/tests/mock_test.py
@@ -1,7 +1,14 @@
-import pytest
+import pytest 
+from os import getenv
 
-def test_mock():
-    try:
-        assert True is True
-    except Exception:
-        pytest.fail("An unexpected exception was raised.")
+def test_env():
+    MONGODB_URI = getenv("MONGODB_URI")
+    MONGODB_USERNAME = getenv("MONGODB_URI")
+    MONGODB_PASSWORD = getenv("MONGODB_URI")
+
+    if MONGODB_URI is None:
+        pytest.fail("Could not retrieve MONGODB_URI")
+    if MONGODB_USERNAME is None:
+        pytest.fail("Could not retrieve MONGODB_USERNAME")
+    if MONGODB_PASSWORD is None:
+        pytest.fail("Could not retrieve MONGODB_PASSWORD")
