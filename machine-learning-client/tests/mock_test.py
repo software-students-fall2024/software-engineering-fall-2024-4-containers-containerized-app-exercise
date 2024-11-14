eb8e7c0d9d34ce84b5fab12e8c32c1dfc7efc920
LucaIgnatescu <lucaignatescu003@gmail.com>
2024-11-14 10:28:12 -0500

    fixed linter errors

--- machine-learning-client/tests/mock_test.py
+++ machine-learning-client/tests/mock_test.py
@@ -1,14 +1,22 @@
-import pytest 
+"""
+This module contains tests for the machine learning client.
+"""
+
 from os import getenv
+import pytest
 
 def test_env():
-    MONGODB_URI = getenv("MONGODB_URI")
-    MONGODB_USERNAME = getenv("MONGODB_URI")
-    MONGODB_PASSWORD = getenv("MONGODB_URI")
+    """
+    Tests the retrieval of required environment variables.
+    Fails if any of the required variables are not set.
+    """
+    mongodb_uri = getenv("MONGODB_URI")
+    mongodb_username = getenv("MONGODB_USERNAME")
+    mongodb_password = getenv("MONGODB_PASSWORD")
 
-    if MONGODB_URI is None:
+    if mongodb_uri is None:
         pytest.fail("Could not retrieve MONGODB_URI")
-    if MONGODB_USERNAME is None:
+    if mongodb_username is None:
         pytest.fail("Could not retrieve MONGODB_USERNAME")
-    if MONGODB_PASSWORD is None:
+    if mongodb_password is None:
         pytest.fail("Could not retrieve MONGODB_PASSWORD")
