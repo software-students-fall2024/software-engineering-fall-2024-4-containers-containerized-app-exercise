ac1e9b6f0fb7487e69c5853e3eb604a0875cfafc
hsualexotake <hsualexotake@gmail.com>
2024-11-15 14:54:42 -0500

    Connected frontend and backend + fixed front end routing issue

--- web-app/app.py
+++ web-app/app.py
@@ -3,27 +3,36 @@ A simple Flask application.
 This module sets up a basic web server using Flask.
 """
 
-
-from flask import Flask, jsonify
+from flask import Flask, render_template, jsonify
 
 # Initialize the Flask app
 app = Flask(__name__)
 
-
 # Define a route for the homepage
-@app.route("/")
+@app.route('/')
 def home():
-    """Returns a JSON message indicating the app is running."""
-    return jsonify(message="Hello, Flask is running in Docker!")
+    """Returns the index.html file."""
+    return render_template('index.html')
 
+# Define a route for the tutorial page
+@app.route("/tutorial")
+def tutorial():
+    """Returns the tutorial.html file."""
+    return render_template("tutorial.html")
 
-# Define another sample route
+# Define a route for the game page
+@app.route("/game")
+def game():
+    """Returns the game.html file."""
+    return render_template("game.html")
+
+# Define another sample route for JSON data
 @app.route("/data")
 def data():
     """Returns a JSON of the data."""
     sample_data = {
         "name": "Dockerized Flask App",
-        "description": "This is a  Flask app running inside Docker.",
+        "description": "This is a Flask app running inside Docker.",
     }
     return jsonify(sample_data)
 
