fbefcfbc629ae9e89037b9b18241612333650926
xc528 <xc2344@nyu.edu>
2024-11-15 12:27:10 -0500

    test pylint

--- machine-learning-client/test_app.py
+++ machine-learning-client/test_app.py
@@ -1,24 +1,26 @@
 """
 This module contains unit tests for ml-client.
 """
-from unittest.mock import MagicMock, patch
+
+# from unittest.mock import MagicMock, patch
 import pytest
-#from flask import Flask
+
+# from flask import Flask
 from app import app
 
 
 @pytest.fixture
-def test_client():
+def client():
     """Fixture for the Flask test client."""
     app.config["TESTING"] = True
-    with app.test_client() as test_client:
-        yield test_client
+    with app.test_client() as client: # pylint: disable=redefined-outer-name
+        yield client
 
 
-@patch("app.sr.Recognizer")
-def test_transcribe_no_audio(mock_recognizer, test_client):
+# @patch("app.sr.Recognizer")
+def test_transcribe_no_audio(client): # pylint: disable=redefined-outer-name
     """Test the /transcribe endpoint with no audio file provided."""
-    response = test_client.post("/transcribe", content_type="multipart/form-data")
+    response = client.post("/transcribe", content_type="multipart/form-data")
 
     assert response.status_code == 200
     assert response.json == {
