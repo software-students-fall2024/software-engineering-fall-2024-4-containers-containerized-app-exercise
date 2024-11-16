4458f2452cd959d68488ba2dd90cb6a771dbcfbc
SewonKim0 <sewonkim0black@gmail.com>
2024-11-16 16:21:29 -0500

    Web-app: gh-actions run for pytest attempt 1

---
+++ web-app/tests/_test.py
@@ -0,0 +1,12 @@
+"""
+This module contains unit tests for the application.
+"""
+
+# import pytest
+
+# TEST
+def test_example():
+    """
+    Test that 1 is equal to 1.
+    """
+    assert 1 == 1
