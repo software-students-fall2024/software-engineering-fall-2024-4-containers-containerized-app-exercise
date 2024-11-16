a1de2562e63e7a8e594387a09cd042d3cb6489ed
Popilopi168 <123021480+Popilopi168@users.noreply.github.com>
2024-11-16 01:23:31 -0500

    lint error update

--- machine-learning-client/ml_client.py
+++ machine-learning-client/ml_client.py
@@ -2,9 +2,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/status', methods=['GET'])
+
+@app.route("/status", methods=["GET"])
 def status():
     return jsonify({"message": "ML Client is running"}), 200
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=6000)
