438259be532dfa4e3740fadf3d11d505b2e385e4
mkphung29 <mkp6075@nyu.edu>
2024-11-16 16:14:28 -0500

    Set up database for frontend

--- web-app/app.py
+++ web-app/app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient("mongodb://localhost:27017/")
+db = client["swearDB"] 
+swears_collection = db["swears"]  
 
 @app.route("/")
 def index():
-    return render_template("index.html", swears=0)
+    total_swears = sum(doc["count"] for doc in swears_collection.find())
+    return render_template("index.html", swears=total_swears)
+
+@app.route("/api/swears", methods=["GET"])
+def get_swear_counts():
+    swear_counts = {doc["word"]: doc["count"] for doc in swears_collection.find()}
+    return jsonify(swear_counts)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
