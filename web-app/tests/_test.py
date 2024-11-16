c51dd7a50a61b9ed52e1287351186766562d252e
SewonKim0 <sewonkim0black@gmail.com>
2024-11-16 17:06:33 -0500

    Web-app: flask pytest fixture setup & bson error resolved

--- web-app/tests/_test.py
+++ web-app/tests/_test.py
@@ -2,12 +2,20 @@
 This module contains unit tests for the application.
 """
 
-# import pytest
+import pytest
+
+from app import create_app
 
 
 # TEST
-def test_example():
-    """
-    Test that 1 is equal to 1.
-    """
+def test_function():
+    """ sample test """
     assert True
+
+@pytest.fixture
+def client():
+    """Fixture for the Flask test client."""
+    app = create_app()
+    app.config["TESTING"] = True
+    with app.test_client() as client:  # pylint: disable=redefined-outer-name
+        yield client
