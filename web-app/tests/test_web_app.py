d695bf91619c3c0ce323c237d25e1372e03f6c33
nehamagesh <neha3magesh@gmail.com>
2024-11-17 17:33:27 -0500

    more unit tests for webapp

--- web-app/tests/test_web_app.py
+++ web-app/tests/test_web_app.py
@@ -15,10 +15,17 @@ def test_record_route(client):
 
 
 @patch("app.audio_collection.insert_one")
+def test_missing_filename(mock_audio, client):
+    file_info = (BytesIO(b"fake_audio_data"), "test_audio.wav")
+    response = client.post("/upload-audio", data={"audio": file_info})
+    assert response.status_code == 400
+    assert not mock_audio.called
+
+
+@patch("app.audio_collection.insert_one")
 @patch("app.requests.get")
 @patch("app.metadata_collection.insert_one")
-def test_upload_audio_success(mock_req, mock_md, mock_audio, client):
-
+def test_upload_audio(mock_req, mock_md, mock_audio, client):
     mock_audio.return_value.inserted_id = "mock_file"
     mock_md.return_value.acknowledged = True
     mock_req.return_value.ok = True
@@ -30,11 +37,28 @@ def test_upload_audio_success(mock_req, mock_md, mock_audio, client):
     file_info = (BytesIO(b"fake_audio_data"), "test_audio.wav")
     response = client.post("/upload-audio", data={"audio": file_info, **data})
 
-    assert response.status_code == 302  # Expecting a redirect to index
+    assert response.status_code == 302
     assert mock_audio.called
     assert mock_md.called
     assert mock_req.called
 
 
-if __name__ == '__main__':
-    unittest.main()
+@patch("app.audio_collection.insert_one")
+@patch("app.metadata_collection.insert_one")
+def test_db_failure(mock_md, mock_audio, client):
+    mock_audio.return_value.inserted_id = None
+
+    data = {
+        "name": "test_audio",
+    }
+    file_info = (BytesIO(b"fake_audio_data"), "test_audio.wav")
+    response = client.post("/upload-audio", data={"audio": file_info, **data})
+
+    assert response.status_code == 500
+
+
+@patch("app.audio_collection.insert_one")
+def test_audio_missing(mock_audio, client):
+    response = client.post("/upload-audio", data={"name": "test_audio"})
+    assert response.status_code == 400
+    assert not mock_audio.called
