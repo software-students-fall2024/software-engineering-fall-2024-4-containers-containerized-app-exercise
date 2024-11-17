95bfd19201b4b3238ec240dd11592eca118a6612
nehamagesh <neha3magesh@gmail.com>
2024-11-17 18:42:17 -0500

    formatting

--- machine-learning-client/tests/test_ml_client.py
+++ machine-learning-client/tests/test_ml_client.py
@@ -1,10 +1,3 @@
-import unittest
-
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
-
-if __name__ == '__main__':
-    unittest.main()
+"""
+This is the unit tests for the ML client
+"""
