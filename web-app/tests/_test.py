7edb6f59909ff3f27f25de30268d9434be444525
SewonKim0 <sewonkim0black@gmail.com>
2024-11-16 23:55:37 -0500

    Web-app: gh-actions code format error fix attempt 1

--- web-app/tests/_test.py
+++ web-app/tests/_test.py
@@ -4,32 +4,32 @@ This module contains unit tests for the application.
 
 import pytest
 
-from app import create_app # pylint: disable=import-error
+from app import create_app  # pylint: disable=import-error
 
 
 @pytest.fixture
-def client(): # pylint: disable=redefined-outer-name
+def client():  # pylint: disable=redefined-outer-name
     """Fixture for the Flask test client."""
     app = create_app()
     app.config["TESTING"] = True
     with app.test_client() as client:  # pylint: disable=redefined-outer-name
         yield client
 
-def test_root1(client): # pylint: disable=redefined-outer-name
+def test_root1(client):  # pylint: disable=redefined-outer-name
     """Test the / route on GET req"""
     response = client.get("/")
-    html_text = response.data.decode('utf-8')
+    html_text = response.data.decode("utf-8")
     assert "Start Recording" in html_text
     assert response.status_code == 200
 
-def test_root2(client): # pylint: disable=redefined-outer-name
+def test_root2(client):  # pylint: disable=redefined-outer-name
     """Test the / route on GET req"""
     response = client.get("/")
-    html_text = response.data.decode('utf-8')
+    html_text = response.data.decode("utf-8")
     assert "Waiting for permission to access the microphone..." in html_text
     assert response.status_code == 200
 
-def test_404(client): # pylint: disable=redefined-outer-name
+def test_404(client):  # pylint: disable=redefined-outer-name
     """Test a non-existent route, expecting 404 error"""
     response = client.get("/non-existent-route")
     assert response.status_code == 404
