181125b4f3d243ba489678d0687b32c4480d1901
SewonKim0 <sewonkim0black@gmail.com>
2024-11-16 23:40:41 -0500

    Web-app: flask routes tests complete

--- web-app/tests/_test.py
+++ web-app/tests/_test.py
@@ -7,11 +7,6 @@ import pytest
 from app import create_app
 
 
-# TEST
-def test_function():
-    """ sample test """
-    assert True
-
 @pytest.fixture
 def client():
     """Fixture for the Flask test client."""
@@ -19,3 +14,22 @@ def client():
     app.config["TESTING"] = True
     with app.test_client() as client:  # pylint: disable=redefined-outer-name
         yield client
+
+def test_root1(client):
+    """Test the / route on GET req"""
+    response = client.get("/")
+    html_text = response.data.decode('utf-8')
+    assert "Start Recording" in html_text
+    assert response.status_code == 200
+
+def test_root2(client):
+    """Test the / route on GET req"""
+    response = client.get("/")
+    html_text = response.data.decode('utf-8')
+    assert "Waiting for permission to access the microphone..." in html_text
+    assert response.status_code == 200
+
+def test_404(client):
+    """Test a non-existent route, expecting 404 error"""
+    response = client.get("/non-existent-route")
+    assert response.status_code == 404
