b0a6c14e33c4aa113c4f7ce2efd4815993340b7b
Popilopi168 <123021480+Popilopi168@users.noreply.github.com>
2024-11-17 01:11:11 -0500

    test

--- machine-learning-client/ml_client.py
+++ machine-learning-client/ml_client.py
@@ -1,21 +1,21 @@
-# """ML Client Flask Application.
+"""ML Client Flask Application.
 
-# This module sets up a simple Flask application that provides endpoints
-# to interact with machine learning services.
-# """
+This module sets up a simple Flask application that provides endpoints
+to interact with machine learning services.
+"""
 
-# from flask import Flask, jsonify
+from flask import Flask, jsonify
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route("/status", methods=["GET"])
-# def status():
-#     """Check the status of the ML Client.
+@app.route("/status", methods=["GET"])
+def status():
+    """Check the status of the ML Client.
 
-#     Returns:
-#         tuple: A tuple containing a JSON response and an HTTP status code.
-#     """
-#     return jsonify({"message": "ML Client is running"}), 200
+    Returns:
+        tuple: A tuple containing a JSON response and an HTTP status code.
+    """
+    return jsonify({"message": "ML Client is running"}), 200
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=6000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=6000)
