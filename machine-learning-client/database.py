b1019087c818883aa9169fd29c102ae7ec2928d5
yz6973 <157756166+yz6973@users.noreply.github.com>
2024-11-17 17:27:20 -0500

    Create database.py

---
+++ machine-learning-client/database.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get MongoDB URI from environment variables
+MONGODB_URI = os.getenv("MONGODB_URI")
+
+# Establish a MongoDB client
+client = MongoClient(MONGODB_URI)
+
+db = client['Its_over_again_database']
