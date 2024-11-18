4dc1e12ebf644ccd75dc031dc80b535da859a204
saharbueno <saharbueno@gmail.com>
2024-11-17 23:39:51 -0500

    connected to db, did styling for login pages, connected to character ai

--- web-app/app.py
+++ web-app/app.py
@@ -1,13 +1,40 @@
+import os
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, redirect, url_for
 from characterai import aiocai, pycai, sendCode, authUser
 import asyncio
 import pymongo
+from bson import ObjectId
+import certifi
 
 email = ''
 code = ''
 client = ''
 
 def create_app():
+
+    load_dotenv()
+
+    mongo_uri = os.getenv("MONGO_DB_URI")
+
+    if mongo_uri is None:
+        raise ValueError("Could not connect to database. Make sure .env is properly configured.")
+    
+    mongo_cli = pymongo.MongoClient(mongo_uri, tls=True, tlsCAFile=certifi.where())
+
+    try:
+        mongo_cli.admin.command('ping')  
+        print("Successfully connected to MongoDB!")
+    except Exception as e:
+        print(f"Failed to connect to MongoDB: {e}")
+
+    db = mongo_cli['hellokittyai_db']
+    users = db['users']
+    characters = db['characters']
+    chats = db['chats']
+
+
     app = Flask(__name__)
 
     @app.route('/', methods=['GET', 'POST'])
@@ -17,7 +44,12 @@ def create_app():
             email = request.form['email']
             if not email:
                 return "Email address is required", 400
-            print(email)
+            
+            user = users.find_one({"email": email})
+            if not user:
+                user_id = str(ObjectId())
+                users.insert_one({"user_id": user_id, "email": email, "chat_history": []})
+                print(f"New user created: {email}")
             code = sendCode(email)
             return render_template('auth.html', address=email)
         
