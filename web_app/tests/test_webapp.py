ec050ad36026c7d8d3aea0b732bccde8cc8e09f3
leonaurdo <leozheng06@gmail.com>
2024-11-17 21:24:10 -0500

    added username, email, and password to face recognition sign-in v2

--- web_app/tests/test_webapp.py
+++ web_app/tests/test_webapp.py
@@ -26,8 +26,8 @@ def _test_client():
 
 def generate_image_data():
     """Helper function to generate fake base64 image data."""
-    return (
-        "data:image/jpeg;base64," + base64.b64encode(b"test_image_data").decode("utf-8")
+    return "data:image/jpeg;base64," + base64.b64encode(b"test_image_data").decode(
+        "utf-8"
     )
 
 
@@ -92,9 +92,7 @@ def test_signup_post_existing_username(mock_users_collection, test_client):
 
 @patch("web_app.web_app.users_collection")
 @patch("web_app.web_app.requests.post")
-def test_signup_post_valid_data(
-    mock_requests_post, mock_users_collection, test_client
-):
+def test_signup_post_valid_data(mock_requests_post, mock_users_collection, test_client):
     """Test the POST request for the signup page with valid data."""
     # Mock users_collection.find_one to return None (no existing user)
     mock_users_collection.find_one.return_value = None
