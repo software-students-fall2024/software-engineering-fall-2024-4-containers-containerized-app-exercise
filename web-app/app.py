5ce0837246a2d5659ff1e888c4d8d0cedcac2bb7
nht251 <nht251@nyu.edu>
2024-11-19 00:23:12 +0200

    lint

--- web-app/app.py
+++ web-app/app.py
@@ -5,7 +5,15 @@ This module sets up the Flask application for the Plant Identifier project.
 import os
 from bson import ObjectId
 from dotenv import load_dotenv
-from flask import Flask, request, render_template, redirect, make_response, session, url_for
+from flask import (
+    Flask,
+    request,
+    render_template,
+    redirect,
+    make_response,
+    session,
+    url_for,
+)
 import pymongo
 
 # from werkzeug.utils import secure_filename
@@ -27,15 +35,13 @@ def create_app():
         if request.args.get("user"):
             user_entries = list(db.plants.find())
             if len(user_entries) > 3:
-                new_entries = [
-                    user_entries[-1],
-                    user_entries[-2],
-                    user_entries[-3]
-                ]
+                new_entries = [user_entries[-1], user_entries[-2], user_entries[-3]]
                 user_entries = new_entries
-            return render_template("home.html", user=request.args.get("user"), user_entries=user_entries)
+            return render_template(
+                "home.html", user=request.args.get("user"), user_entries=user_entries
+            )
         return render_template("home.html")
-    
+
     @app.route("/login", methods=["GET", "POST"])
     def login():
         if request.method == "POST":
@@ -43,22 +49,22 @@ def create_app():
             password = request.form["password"]
 
             session["username"] = username
-            
+
             return redirect(url_for("home", user=username))
-        
+
         return render_template("login.html")
-    
+
     @app.route("/signup", methods=["GET", "POST"])
     def signup():
         if request.method == "POST":
             username = request.form["username"]
             password = request.form["password"]
 
-            db.users.insert_one({"username":username, "password":password})
+            db.users.insert_one({"username": username, "password": password})
             session["username"] = username
 
             return redirect(url_for("home", user=username))
-        
+
         return render_template("signup.html")
 
     @app.route("/upload", methods=["GET", "POST"])
@@ -67,27 +73,31 @@ def create_app():
             plant_photo = request.form["photo"]
             plant_name = "placeholder"
             plant_data = {
-                "photo":plant_photo,
-                "name":plant_name,
-                "user":session["username"]
+                "photo": plant_photo,
+                "name": plant_name,
+                "user": session["username"],
             }
             new_entry = db.plants.insert_one(plant_data)
             new_entry_id = new_entry.inserted_id
             return redirect(url_for("new_entry", new_entry_id=new_entry_id))
-        return render_template('upload.html')
+        return render_template("upload.html")
 
     @app.route("/new_entry", methods=["GET", "POST"])
     def new_entry():
-        new_entry_id = request.args.get('new_entry_id')
+        new_entry_id = request.args.get("new_entry_id")
         entry = ObjectId(new_entry_id)
         if request.method == "POST":
             instructions = request.form["instructions"]
-            db.plants.update_one({"_id":entry}, {'$set': {"instructions": instructions}})
-            return(redirect(url_for("home", user=session["username"])))
-        document = db.plants.find_one({"_id":entry})
+            db.plants.update_one(
+                {"_id": entry}, {"$set": {"instructions": instructions}}
+            )
+            return redirect(url_for("home", user=session["username"]))
+        document = db.plants.find_one({"_id": entry})
         photo = document["photo"]
         name = document["name"]
-        return render_template('new-entry.html', photo=photo, name=name, new_entry_id=new_entry_id)
+        return render_template(
+            "new-entry.html", photo=photo, name=name, new_entry_id=new_entry_id
+        )
 
     @app.route("/results/<filename>")
     def results(filename):
@@ -98,7 +108,7 @@ def create_app():
 
     @app.route("/history")
     def history():
-        all_results = list(db.plants.find({"user":session["username"]}))
+        all_results = list(db.plants.find({"user": session["username"]}))
         return render_template("history.html", all_results=all_results)
 
     return app
