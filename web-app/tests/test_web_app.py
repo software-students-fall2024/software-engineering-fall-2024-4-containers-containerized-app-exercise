ce6555c028d9c5177cfc3513fca108e525d19e2b
LucaIgnatescu <lucaignatescu003@gmail.com>
2024-11-18 21:18:19 -0500

    updated directory structure

--- web-app/tests/test_web_app.py
+++ web-app/tests/test_web_app.py
@@ -1,54 +1,40 @@
 """
-This file contains tests for the web app
+This is the tests for the webapp
 """
 
 from io import BytesIO
+
+# pylint: disable=unused-import
 from unittest.mock import patch
+
+# pylint: disable=unused-import
 import pytest
-from web_app.app import app, audio_collection, metadata_collection
+
+# pylint: disable=unused-import
+# pylint: disable=import-error
+from src.app import app, audio_collection, metadata_collection
 
 
-def mock_test():
-    """
-    Mock test
-    """
-    assert True
-
-
-def test_index(client):
-    """
-    Test the index route
-    """
-    response = client.get("/")
+def test_record_route(test_client):
+    """Test that the record route '/' works."""
+    response = test_client.get("/record")
     assert response.status_code == 200
 
 
-def test_record_route(client):
-    """
-    Test the record route
-    """
-    response = client.get("/record")
-    assert response.status_code == 200
-
-
-@patch("app.audio_collection.insert_one")
-def test_missing_filename(mock_audio, client):
-    """
-    Test the app with a missing filename
-    """
+@patch("src.app.audio_collection.insert_one")
+def test_missing_filename(mock_audio, test_client):
+    """Test missing filename"""
     file_info = (BytesIO(b"fake_audio_data"), "test_audio.wav")
-    response = client.post("/upload-audio", data={"audio": file_info})
+    response = test_client.post("/upload-audio", data={"audio": file_info})
     assert response.status_code == 400
     assert not mock_audio.called
 
 
-@patch("app.audio_collection.insert_one")
-@patch("app.requests.get")
-@patch("app.metadata_collection.insert_one")
-def test_upload_audio(mock_req, mock_md, mock_audio, client):
-    """
-    Test the app with a successful file
-    """
+@patch("src.app.audio_collection.insert_one")
+@patch("src.app.requests.get")
+@patch("src.app.metadata_collection.insert_one")
+def test_upload_audio(mock_req, mock_md, mock_audio, test_client):
+    """Test uploading audio"""
     mock_audio.return_value.inserted_id = "mock_file"
     mock_md.return_value.acknowledged = True
     mock_req.return_value.ok = True
@@ -58,7 +44,7 @@ def test_upload_audio(mock_req, mock_md, mock_audio, client):
         "name": "test_audio",
     }
     file_info = (BytesIO(b"fake_audio_data"), "test_audio.wav")
-    response = client.post("/upload-audio", data={"audio": file_info, **data})
+    response = test_client.post("/upload-audio", data={"audio": file_info, **data})
 
     assert response.status_code == 302
     assert mock_audio.called
@@ -66,28 +52,26 @@ def test_upload_audio(mock_req, mock_md, mock_audio, client):
     assert mock_req.called
 
 
-@patch("app.audio_collection.insert_one")
-@patch("app.metadata_collection.insert_one")
-def test_db_failure(mock_audio, client):
-    """
-    Test the app with a failing db
-    """
-    mock_audio.return_value.inserted_id = None
+@patch("src.app.audio_collection.insert_one")
+@patch("src.app.metadata_collection.insert_one")
+def test_db_failure(mock_md, mock_audio, test_client):
+    """Test failure when database insertion fails"""
+    mock_audio.return_value.inserted_id = None  # Simulating the failure
+    mock_md.return_value.acknowledged = False  # Simulating failure in metadata insert
 
     data = {
         "name": "test_audio",
     }
     file_info = (BytesIO(b"fake_audio_data"), "test_audio.wav")
-    response = client.post("/upload-audio", data={"audio": file_info, **data})
+
+    response = test_client.post("/upload-audio", data={"audio": file_info, **data})
 
     assert response.status_code == 500
 
 
-@patch("app.audio_collection.insert_one")
-def test_audio_missing(mock_audio, client):
-    """
-    Test the app with no audio
-    """
-    response = client.post("/upload-audio", data={"name": "test_audio"})
+@patch("src.app.audio_collection.insert_one")
+def test_audio_missing(mock_audio, test_client):
+    """test without audio"""
+    response = test_client.post("/upload-audio", data={"name": "test_audio"})
     assert response.status_code == 400
     assert not mock_audio.called
