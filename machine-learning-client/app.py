ff8c596eeffc13878faeeb08be285c3296a1ae9e
AnHaii <ah6253@nyu.edu>
2024-11-19 23:03:33 -0500

    docker

--- machine-learning-client/app.py
+++ machine-learning-client/app.py
@@ -3,17 +3,15 @@ import numpy as np
 import time
 
 def collect_and_analyze_data():
-    # 连接到 MongoDB 数据库
     client = MongoClient("mongodb://mongodb:27017/")
     db = client.ml_data
     collection = db.results
 
-    # 模拟数据收集和分析
     while True:
         data = {"values": list(np.random.rand(5))}
         collection.insert_one(data)
         print("Data inserted:", data)
-        time.sleep(10)  # 每 10 秒插入一次数据
+        time.sleep(10) 
 
 if __name__ == "__main__":
     collect_and_analyze_data()
