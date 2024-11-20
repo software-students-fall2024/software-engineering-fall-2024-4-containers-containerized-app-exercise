696f12ce579917e677214dce1245cb6c8c48a50b
jh7316 <jh7316@nyu.edu>
2024-11-19 22:15:06 -0500

    refactor & format

--- web_app/app.py
+++ web_app/app.py
@@ -21,8 +21,8 @@ def create_app():
 
     db = client.asl_db
 
-    if client is None:
-        raise ValueError("Error with URI")
+    # if client is None:
+    # raise ValueError("Error with URI")
 
     # try:
     #     client = MongoClient(client)
@@ -46,25 +46,26 @@ def create_app():
             if not image_data:
                 return jsonify({"error": "No image data received"}), 400
             image_data = image_data.split(",")[1]
-            image_binary = image_data  # base64.b64decode(image_data)
-
-            image_doc = {"image": image_binary, "output": "", "translation": ""}
-            result = db.images.insert_one(image_doc)
-
-            image_id = str(result.inserted_id)
-            ml_client_url = "http://ml_client:5001/processImage"
-            response = requests.post(ml_client_url, json={"image_id": str(image_id)})
-            result = db.images.find_one({"_id": ObjectId(image_id)})
-
+            response = requests.post(
+                "http://ml_client:5001/processImage",
+                json={
+                    "image_id": str(
+                        db.images.insert_one(
+                            {"image": image_data, "output": "", "translation": ""}
+                        ).inserted_id
+                    )
+                },
+            )
             if response.status_code == 200:
                 data = response.json()
-                output = data.get("output")
-                translation = data.get("translation")
                 return jsonify(
-                    {"status": "success", "output": output, "translation": translation}
+                    {
+                        "status": "success",
+                        "output": data.get("output"),
+                        "translation": data.get("translation"),
+                    }
                 )  # redirect(url_for("display_images"))
-            else:
-                return "Error processing image"
+            return "Error processing image"
 
         except Exception as e:
             print(f"Error: {e}")
@@ -86,28 +87,27 @@ def create_app():
     #     images = db.images.find()
     #     return render_template("display_images.html", images=images)
 
-    @app.route("/upload_image", methods=["POST"])
-    def upload_image():
-        image_data = request.form["image_data"]
-        if image_data != "test":
-            image_id = db.images.insert_one({"image_data": image_data}).inserted_id
-            ml_client_url = "http://ml_client/processImage"
-            response = requests.post(ml_client_url, json={"image_id": str(image_id)})
+    # @app.route("/upload_image", methods=["POST"])
+    # def upload_image():
+    #     image_data = request.form["image_data"]
+    #     if image_data != "test":
+    #         image_id = db.images.insert_one({"image_data": image_data}).inserted_id
+    #         ml_client_url = "http://ml_client/processImage"
+    #         response = requests.post(ml_client_url, json={"image_id": str(image_id)})
 
-            # if response.status_code == 200:
-            #     return redirect(url_for("display_images"))
-            # else:
-            #     return "Error processing image"
+    # if response.status_code == 200:
+    #     return redirect(url_for("display_images"))
+    # else:
+    #     return "Error processing image"
 
-    def get_data():
+    # def get_data():
 
-        # TO DO
+    #     # TO DO
 
-        return
+    #     return
 
     return app
 
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run(host="0.0.0.0", port=5002, debug=True)
+    create_app().run(host="0.0.0.0", port=5002, debug=True)
