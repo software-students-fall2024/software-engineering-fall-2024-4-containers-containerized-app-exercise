f872f4240a98622403809f2de675ce09bca98f69
yeshnii <ys4823@nyu.edu>
2024-11-20 14:50:30 -0500

    fixed HTML code in fileUpload.html template and style.css

--- web-app/app.py
+++ web-app/app.py
@@ -1,22 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    if 'files[]' in request.files:
+        file = request.files['files[]']
+        file.save(file.filename)
+        session['audiofile'] = file
+        return redirect(url_for('uploaded'))
+        # return render_template('uploaded.html')
     return render_template('fileUpload.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    if 'files[]' in request.files:
-        file = request.files['files[]']
-        file.save(file.filename)
-        return render_template('uploaded.html')
-    return 'No file uploaded'
-
+    return render_template('uploaded.html')
+                           
 @app.route('/spoken')
 def spoken():
-    return render_template('spoken.html')
+    audio_to_text(100)
+    return render_template('spoken.html', "The transcript is here.")
 
 
 if __name__ == '__main__':
