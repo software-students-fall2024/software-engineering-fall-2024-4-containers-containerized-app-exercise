88b1247c40575eb764bc7ea706a346cb3e221fec
mkphung29 <mkp6075@nyu.edu>
2024-11-21 13:17:03 -0500

    Format it with black

--- web-app/app.py
+++ web-app/app.py
@@ -12,6 +12,7 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client["swearDB"]
 swears_collection = db["swears"]
 
+
 @app.route("/")
 def index():
     """
@@ -19,6 +20,7 @@ def index():
     """
     total_swears = sum(doc["count"] for doc in swears_collection.find())
     return render_template("index.html", swears=total_swears)
+
 
 @app.route("/api/swears", methods=["GET"])
 def get_swear_counts():
@@ -30,6 +32,7 @@ def get_swear_counts():
         print(doc)
     swear_counts = {doc["word"]: doc["count"] for doc in swears_collection.find()}
     return jsonify(swear_counts)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
