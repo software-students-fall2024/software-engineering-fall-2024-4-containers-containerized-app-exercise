07aac08f2d1a5d832969b7f0fca25a624655cdf3
mkphung29 <mkp6075@nyu.edu>
2024-11-21 13:13:36 -0500

    Fix lint errors

--- web-app/app.py
+++ web-app/app.py
@@ -1,18 +1,30 @@
+"""
+This is the main application file for the Swear Jar app.
+
+It uses Flask to serve an HTML template and a REST API for swear counts.
+Data is stored in a MongoDB database.
+"""
 from flask import Flask, render_template, jsonify
 from pymongo import MongoClient
 
 app = Flask(__name__)
 client = MongoClient("mongodb://localhost:27017/")
-db = client["swearDB"] 
-swears_collection = db["swears"]  
+db = client["swearDB"]
+swears_collection = db["swears"]
 
 @app.route("/")
 def index():
+    """
+    Render the homepage with the total number of swears.
+    """
     total_swears = sum(doc["count"] for doc in swears_collection.find())
     return render_template("index.html", swears=total_swears)
 
 @app.route("/api/swears", methods=["GET"])
 def get_swear_counts():
+    """
+    Return the swear counts
+    """
     docs = swears_collection.find()
     for doc in docs:
         print(doc)
